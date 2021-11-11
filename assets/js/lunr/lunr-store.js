5ddb27f32e329de11e954061ef321fa0ffa9e778
Pasi Kilponen <pasi@Pasis-MacBook-Pro.local>
2021-11-11 16:15:51 +0200

    update suljettu post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Kalastusluvat",
-        "excerpt":"Meidän kautta saat paikallisen kalastuskunnan kalastusluvat Lentualle sekä Nivan koskialueelle.    ","categories": ["jekyll","update"],
+        "title": "Ensi kauden varauksia vastaan otetaan",
+        "excerpt":"Otamme vastaan varauksia ensi kaudelle. Pidämme mökit kiinni nyt talvikaudella ja avaamme jälleen keväällä 2022 toukokuun alussa.    ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/kalastus/",
-        "teaser": null
-      },{
-        "title": "Metsästys",
-        "excerpt":"Kanalinnustuslupia voi alkaa jälleen ostaa maanantaina 30.8. alkavalla viikolla. Meiltä myös luvat Timonienen metsästysseuran alueelle.  Varaa ajoissa.    ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/metsastys/",
+        "url": "/jekyll/update/suljettu/",
         "teaser": null
       }]
