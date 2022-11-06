0c3521b8313aed466618fee67225fea7b99d114f
Pasi Kilponen <pasi@Pasis-MacBook-Pro-2.local>
2022-11-06 11:40:25 +0200

    update closed for 2022

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,7 @@
 var store = [{
-        "title": "Kesäkausi 2022 nyt varattavissa.",
-        "excerpt":"Nyt vielä hyvin vapaita viikkoja, vuokraa nyt rantamökki Kuhmosta Lentua järven rannalta!    ","categories": ["jekyll","update"],
+        "title": "Ensi kauden varauksia vastaan otetaan",
+        "excerpt":"Otamme vastaan varauksia ensi kaudelle. Pidämme mökit kiinni nyt talvikaudella ja avaamme jälleen keväällä 2023 toukokuun alussa.    ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/avattu/",
-        "teaser": null
-      },{
-        "title": "Kalastusluvat",
-        "excerpt":"Meidän kautta saat paikallisen kalastuskunnan kalastusluvat Lentualle sekä Nivan koskialueelle.    ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/kalastus/",
-        "teaser": null
-      },{
-        "title": "Metsästys",
-        "excerpt":"Pohjanmaa ja Kainuu alueen kanalinnustuslupia ovat ostettavissa eräluvat palvelussa 10.6 ja 2.9 alkaen. Meiltä myös luvat Timoniemen metsästysseuran alueelle.    ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/metsastys/",
+        "url": "/jekyll/update/suljettu/",
         "teaser": null
       }]
