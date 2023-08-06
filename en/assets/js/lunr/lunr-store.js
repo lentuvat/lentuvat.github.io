ef9257d0b565aca256879e8cf5d9b4bbefd89eae
Pasi Kilponen <pasi.kilponen@tietoevry.com>
2023-08-06 11:14:26 +0300

    english translations added

---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Summer 2023 open.",
+        "excerpt":"Now it is good time to reserve very nice accomodation at cottages by the lake!    ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/en/jekyll/update/avattu/",
+        "teaser": null
+      },{
+        "title": "Kalastusluvat",
+        "excerpt":"From us you can also get fishing permits to the local Lentua area and to the Niva area.    ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/en/jekyll/update/kalastus/",
+        "teaser": null
+      }]
