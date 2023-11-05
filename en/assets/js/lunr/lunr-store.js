40c121cbdcf43a08a3ed3e7741e1b2cf8246312f
Pasi Kilponen <pasi.kilponen@tietoevry.com>
2023-11-05 11:13:59 +0200

    closed for season 2023 update

--- en/assets/js/lunr/lunr-store.js
+++ en/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Summer 2023 open.",
-        "excerpt":"Now it is good time to reserve very nice accomodation at cottages by the lake!    ","categories": ["jekyll","update"],
+        "title": "Reservations to next season",
+        "excerpt":"Now it is time to do reservation for next season. We are closed for winter time and open again at May 2024.    ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/en/jekyll/update/avattu/",
-        "teaser": null
-      },{
-        "title": "Kalastusluvat",
-        "excerpt":"From us you can also get fishing permits to the local Lentua area and to the Niva area.    ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/en/jekyll/update/kalastus/",
+        "url": "/en/jekyll/update/suljettu/",
         "teaser": null
       }]
