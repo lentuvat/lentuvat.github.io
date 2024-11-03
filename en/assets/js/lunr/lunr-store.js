5ed668d700f1140d250146e16176396ec6012979
Pasi Kilponen <pasi@user-MacBook-Pro.local>
2024-11-03 15:06:15 +0200

    closed update

--- en/assets/js/lunr/lunr-store.js
+++ en/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Summer 2024 open.",
-        "excerpt":"Now it is good time to reserve very nice accomodation at cottages by the lake!    ","categories": ["jekyll","update"],
+        "title": "Next season accomodation reservations",
+        "excerpt":"We are now closed for the winter time and taking reservations for next season that opens at May 2025.    ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/en/jekyll/update/avattu/",
-        "teaser": null
-      },{
-        "title": "Kalastusluvat",
-        "excerpt":"From us you can also get fishing permits to the local Lentua area and to the Niva area.    ","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/en/jekyll/update/kalastus/",
+        "url": "/en/jekyll/update/suljettu/",
         "teaser": null
       }]
